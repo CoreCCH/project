92f767dbc02d5a586e2898cee07d83be0d699f49
CoreCCH <j1618james.cm05g@nctu.edu.tw>
2023-04-06 01:04:18 +0800

    ver 0.0

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'schedule',
+        'USER': 'root',
+        'PASSWORD': '2264729t',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
